501dc2efafbe84210b2decf17f03fac2a3085b37
Patricio <115204170+PatricioMGM@users.noreply.github.com>
2024-06-12 19:44:57 -0600

    cosillas

--- proyecto_materia/lib/main.dart
+++ proyecto_materia/lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -11,34 +10,70 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Material App',
       home: Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Inicio',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Buscar',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Perfil',
+            ),
+          ],
+        ),
         appBar: AppBar(
           title: const Text('Material App Bar'),
         ),
         body: Center(
-          child: Column(
-            children: [
-              const Text('Iniciar sesion', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-              const SizedBox(height: 20,),
-              const TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Usuario',
+          child: Container(
+            width: double.infinity,
+            height: double.infinity,
+            margin: const EdgeInsets.symmetric(horizontal: 40),
+            child: Column(
+              mainAxisAlignment:
+                  MainAxisAlignment.center, // Centra verticalmente
+              crossAxisAlignment:
+                  CrossAxisAlignment.center, // Centra horizontalmente
+              children: [
+                const Text(
+                  'Iniciar sesion',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 ),
-              ),
-              const SizedBox(height: 20,),
-              const TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Contraseña',
+                const SizedBox(
+                  height: 20,
                 ),
-              ),
-              const SizedBox(height: 20,),
-              ElevatedButton(
-                onPressed: (){},
-                child: const Text('Iniciar sesion'),
-              ),
-              
-            ],
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(10),),
+                    labelText: 'Usuario',
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(10),),
+                    labelText: 'Contraseña',
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: (){},
+                    child: const Text('Iniciar sesión'),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
